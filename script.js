dda5758805c17c3e5170232e6dd3344b8bdf7273
prachin12 <magarprachin12@gmail.com>
2024-08-27 08:51:21 +1000

    logic not working in js working on it

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let addbk=document.querySelector(".addbk");
 let toaddbk=document.querySelector(".toaddbk");
 let submit=document.querySelector(".submit");
+let library=document.querySelector("#table")
 let mybook=[];
+let n=0;
 let sn=1;
+let i=0;
+
 function Book(sn,title,author,pageno,status){
     this.sn=sn;
     this.title=title;
     this.author=author;
     this.page=pageno;
     this.status=status;
+    
 }
 
 addbk.addEventListener('click',()=>{
@@ -19,14 +24,30 @@ addbk.addEventListener('click',()=>{
 
 
     submit.addEventListener('click',()=>{
-
+        console.log('clicked');
         let book = new Book(sn,toaddbk.title.value, toaddbk.author.value, toaddbk.pages.value, toaddbk.status.value);
         mybook.push(book);
+        let row=document.createElement("tr");
+        
+        let length=Object.keys(mybook[n]).length;
+
+        console.log(mybook[0]);
+        Object.entries(mybook[n]).forEach(([key, value]) => {
+            if(i!=length){
+                let detail=document.createElement("td");
+                detail.innerText=value;
+                i++;
+                row.appendChild(detail);
+            }
+        });
+        library.appendChild(row);
+        console.log(row);
+        i=0;
+        
         event.preventDefault();
 
         toaddbk.classList.add("hide");
         toaddbk.classList.remove("show");
-        
         sn++;
     })
 })
